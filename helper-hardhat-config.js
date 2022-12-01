0d5dff07226718b5a71edf22f32eb8aa692e6417
Sukanya SS <ss.sukanya@gmail.com>
2022-12-01 21:42:34 +0700

    second commit

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -17,6 +17,7 @@ const networkConfig = {
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // doesn't matter since will use mock
         callbackGasLimit: "500000", // 500,000
         interval: "30",
+        //subscriptionId: "4969",
     },
 }
 
